21202a646190b3b0eda6b4913515349469d01acb
Aimed Rab <147007664+Aimed06@users.noreply.github.com>
2025-05-20 15:22:38 +0200

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -5,7 +5,7 @@ export const db = mysql.createPool({
   host: process.env.MYSQL_HOST || "localhost",
   user: process.env.MYSQL_USER || "root",
   password: process.env.MYSQL_PASSWORD || "",
-  database: process.env.MYSQL_DATABASE || "restaurant",
+  database: process.env.MYSQL_DATABASE || "onfrip",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
